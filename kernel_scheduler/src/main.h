6e749613a0089eadffcd917a820d71e818f4e7c0
germanmorenobauer <germanmbauer@gmail.com>
2026-07-15 10:55:47 -0300

    Correcion

--- kernel_scheduler/src/main.h
+++ kernel_scheduler/src/main.h
@@ -37,7 +37,7 @@ typedef struct {
     int fd_cpu;
     int pid_ejcutando;
 } t_cpu_info;
-extern t_lista_cpus_sched;
+extern t_list* lista_cpus_sched;
 extern pthread_mutex_t m_cpus_sched;
 extern int fd_memoria;
 
